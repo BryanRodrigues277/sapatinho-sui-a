aa792a0e0645b4c2a4705e3616aa95dd55fc9675
Bryan <user@email.com>
2026-03-06 10:16:07 -0300

    Update checkout link to Hotmart

--- src/hooks/useCheckoutLink.ts
+++ src/hooks/useCheckoutLink.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { buildURLWithUTM } from '../utils/utm';
 
-const CHECKOUT_BASE_URL = 'https://heloisa-artesa.pay.yampi.com.br/r/21FQTS2FP1';
+const CHECKOUT_BASE_URL = 'https://pay.hotmart.com/H104769986O?checkoutMode=10';
 
 export function useCheckoutLink(): string {
   const checkoutUrl = useMemo(() => {
